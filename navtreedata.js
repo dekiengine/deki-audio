94355ee2be9c9e2363573752972ed15ce8a6132e
Kirbyrawr <devkirbyrawr@gmail.com>
2026-09-20 21:03:55 +0200

    Docs for deki-audio from deki-editor d152da0

--- navtreedata.js
+++ navtreedata.js
@@ -31,8 +31,10 @@ var NAVTREE =
       [ "MAX98357AudioComponent", "index.html#c-max98357audiocomponent", null ]
     ] ],
     [ "Changelog", "index.html#autotoc_md3", [
-      [ "0.15.0", "index.html#autotoc_md4", null ],
-      [ "Changed", "index.html#autotoc_md5", null ]
+      [ "0.16.0", "index.html#autotoc_md4", null ],
+      [ "Changed", "index.html#autotoc_md5", null ],
+      [ "0.15.0", "index.html#autotoc_md6", null ],
+      [ "Changed", "index.html#autotoc_md7", null ]
     ] ]
   ] ]
 ];
